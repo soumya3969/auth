d8d91f2bb7eafae91882b90c144268a1bd94b61a
soumya3969 <srmhnt2001@gmail.com>
2024-09-28 10:08:42 +0530

    files updates

--- test/sample.js
+++ test/sample.js
@@ -1,3 +1,30 @@
-let res = Date.now() + 24 * 60 * 60 * 1000;
-console.log(res.toLocaleString());
-console.log(res);
+let input = prompt("Enter Month :");
+let mon = input.toUpperCase();
+let yer = +prompt("Enter Year :");
+
+switch (mon) {
+  case "JAN" || "MAR" || "MAY" || "JUL" || "AUG" || "OCT" || "DEC":
+    console.log("31 Days");
+    document.write("31 Days");
+    break;
+
+  case "FEB":
+    if ((yer % 4 == 0 && yer % 100 != 0) || yer % 400 == 0) {
+      console.log("29 Days");
+      document.write("29 Days");
+    } else {
+      console.log("28 Days");
+      document.write("28 Days");
+    }
+    break;
+
+  case "APR" || "JUN" || "AUG" || "OCT" || "DEC":
+    console.log("30 Days");
+    document.write("30 Days");
+    break;
+
+  default:
+    console.log("Invalid Month");
+    document.write("Invalid Month");
+    break;
+}
