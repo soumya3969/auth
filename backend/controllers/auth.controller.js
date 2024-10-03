274c83bbb6910324edb55e2875da4204189704bf
soumya3969 <srmhnt2001@gmail.com>
2024-10-03 14:50:46 +0530

    login feature completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,7 +87,40 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("Login Route");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email does not exist"
+      });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect password"
+      });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: null
+      }
+    });
+  } catch (error) {
+    console.log("error in login", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 export const logout = async (req, res) => {
   res.clearCookie("token");
