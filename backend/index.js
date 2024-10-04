4da88866d45f9e3f3c19df0fa43d2604cc1822cf
soumya3969 <srmhnt2001@gmail.com>
2024-10-04 11:37:14 +0530

    Refactor checkAuth endpoint and add middleware for token verification

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./db/connectDB.js";
+
 import authRoutes from "./routes/auth.route.js";
 
-const app = express();
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); //? allows us to parse incoming requests :req.body
+app.use(cookieParser()); //? allows us to parse incoming cookies :req.cookies
 
 app.get("/", (req, res) => {
   res.send("API is running...");
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,12 +5,14 @@ import {
   signup,
   verifyEmail,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  checkAuth
 } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-// router.get("/check-auth", verifyToken, checkAuth); //! work on later stopped at 1.47.00
+router.get("/check-auth", verifyToken, checkAuth); //! work on later stopped at 1.47.00
 
 router.post("/signup", signup);
 router.post("/login", login);
